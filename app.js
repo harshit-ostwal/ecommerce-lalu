7db501dd9b4f161753e9ad411fc4e292be0d9906
Harshit Jain <harshitostwal1234@gmail.com>
2025-01-19 16:28:28 +0530

    123

---
+++ app.js
@@ -0,0 +1,155 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const db = require('./config/database'); 
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const cors = require("cors");
+const session = require('express-session')
+const MemoryStore = require('memorystore')(session)
+const flash = require('connect-flash');
+const app = express();
+const passport = require("passport");
+const http  = require('http');
+const https = require('https');
+const server = http.createServer(app);
+// const LocalStrategy = require("passport-local").Strategy;
+const errorHandler = require('./middlewares/errorHandler');
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+app.set('views', [path.join(__dirname, 'views'), path.join(__dirname, 'views/userViews')]);
+app.set('view engine', 'ejs');
+
+app.use(session({
+  cookie: { maxAge: 86400000 },
+  store: new MemoryStore({
+    checkPeriod: 86400000 // prune expired entries every 24h
+  }),
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: true,
+
+}));
+
+app.use(
+  cors({
+    credentials: true,
+    origin: '*',
+    optionsSuccessStatus: 200,
+  })
+);
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(flash())
+
+
+app.use(passport.initialize());
+
+
+const adminRouter = require('./routes/adminRoute');
+const usersRouter = require('./routes/userRoute');
+const cartRouter = require('./routes/cartRoute');
+// const homeRouter  = require('./routes/homeRoute')
+
+
+require('./config/passport')(passport);
+//passport -1
+
+
+app.use('/admin', adminRouter);
+app.use('/', usersRouter);
+app.use('/cart',cartRouter)
+app.get('/keep-alive', (req, res) => {
+    res.status(200).send('Server is alive!');
+});
+
+
+app.use(errorHandler);
+
+
+function keepAlive() {
+    const options = {
+        hostname: 'f-store-ecommerce.onrender.com',
+        path: '/keep-alive',
+        method: 'GET',
+    };
+
+    const req = https.request(options, (res) => {
+        console.log(`Keep-alive status: ${res.statusCode}`);
+        res.on('data', (d) => process.stdout.write(d));
+    });
+
+    req.on('error', (error) => {
+        console.error(`Error calling keep-alive: ${error.message}`);
+    });
+
+    req.end();
+}
+
+setInterval(keepAlive, 1500000); 
+
+app.use('*',(req,res,next)=>{
+  res.render('errorHandler')
+})
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+
+
+
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+
+  if (port >= 0) {
+    return port;
+  }
+
+  return false;
+}
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+  console.log('Listening on ' + bind); // Replace 'debug' with 'console.log'
+}
+
---
+++ config/database.js
@@ -0,0 +1,18 @@
+// config/database.js
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+// Define the database connection URL
+const databaseURL = process.env.MONGODB_URI ;
+
+// Connect to MongoDB
+mongoose.connect(databaseURL);
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+module.exports = db;
